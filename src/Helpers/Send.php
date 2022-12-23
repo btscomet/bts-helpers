d6f1c2171ef6818bd93ada4da00ebcb034203d25
Juan Carlos Hernández Pérez <j.carlos@btscomet.com>
2022-12-24 00:02:51 +0100

    Send email
    Send email

---
+++ src/Helpers/Send.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Src\Helpers;
+
+class Send
+{
+
+    /**
+     * @param $from
+     * @param $to
+     * @param $subject
+     * @param $message
+     * @return int
+     */
+    public static function email($from, $to, $subject, $message): int
+    {
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        $headers .= 'From: <' . $from . '>' . "\r\n";
+
+        mail($to, $subject, $message, $headers);
+        return 1;
+    }
+
+}
